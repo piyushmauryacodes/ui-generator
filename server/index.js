8c2bf84ee8c22cbe0cbfdba192bc85134d674ddf
piyushmaurya0823-crypto <piyushmaurya0823@gmail.com>
2026-02-12 23:16:18 +0530

    Fix wildcard route syntax error

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options('*', cors());
+app.options(/(.*)/, cors());
 app.use(express.json());
 
 // 1. Database Connection
