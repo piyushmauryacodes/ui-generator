df254eec35b5357003bdaa7cf1b1e5ab16a57709
piyushmaurya0823-crypto <piyushmaurya0823@gmail.com>
2026-02-23 23:56:07 +0530

    update CORS

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://vercel.com/piyushmauryacodes-projects/ui-generator',
+  credentials: true
+})); 
 app.use(express.json());
 
 // Absolute log path
